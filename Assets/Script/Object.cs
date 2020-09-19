bed3d930df7803d429b1621b19a6ee3ec6fbba85
Unno-Ryo <grisiasachi@gmail.com>
2020-09-19 13:17:03 +0900

    no message

---
+++ Assets/Script/Object.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var destroyer = collision.gameObject.GetComponent<Destroyer>();
+
+        
+
+        if (destroyer != null)
+        {
+                Destroy(obj, 3.0f);
+        }
+    }
+}
